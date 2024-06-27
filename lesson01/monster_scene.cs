aa813e1e81d846442fd2c3226acc2dfbc2b37ff0
Piotr Gaszewski <piog@netcompany.com>
2024-06-27 14:32:13 +0200

    Changing position when moving

--- lesson01/monster_scene.cs
+++ lesson01/monster_scene.cs
@@ -3,12 +3,16 @@ using System;
 
 public partial class monster_scene : Sprite2D
 {
+
+	int angleInDegrees = 0; 
+	int radius = 30; 
+	Godot.Sprite2D child; 
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-		var child = this.GetNode<Godot.Sprite2D>("MonsterChildSprite"); 
+		child = this.GetNode<Godot.Sprite2D>("MonsterChildSprite"); 
 		//child.Position = new Vector2(100, 100);
-		child.Position = CalculatePointOnCircle(10, this.Position, 0); 
+		child.Position = CalculatePointOnCircle(30, this.Position, angleInDegrees); 
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -19,18 +23,22 @@ public partial class monster_scene : Sprite2D
 		if (Input.IsKeyPressed(Key.W))
 		{		
 			this.Position += new Vector2(0, -SHIFT);
+			child.Position = CalculatePointOnCircle(radius, this.Position, IncreaseAngle()); 
 		}
 		if (Input.IsKeyPressed(Key.S))
 		{
 			this.Position += new Vector2(0, SHIFT);
+			child.Position = CalculatePointOnCircle(radius, this.Position, IncreaseAngle()); 
 		}
 		if (Input.IsKeyPressed(Key.A))
 		{
 			this.Position += new Vector2(-SHIFT, 0);
+			child.Position = CalculatePointOnCircle(radius, this.Position, IncreaseAngle()); 
 		}
 		if (Input.IsKeyPressed(Key.D))
 		{
 			this.Position += new Vector2(SHIFT, 0);
+			child.Position = CalculatePointOnCircle(radius, this.Position, IncreaseAngle()); 
 		}
 	}
 	public static Vector2 CalculatePointOnCircle(float radius, Vector2 center, float angleInDegrees)
@@ -42,5 +50,15 @@ public partial class monster_scene : Sprite2D
 		float y = center.Y + (float)(radius * Math.Sin(angleInRadians));
 
 		return new Vector2(x, y);
+	}
+
+	public int IncreaseAngle()
+	{
+		angleInDegrees += 1;
+		if(angleInDegrees >= 360)
+		{
+			angleInDegrees = 0;
+		}
+		return angleInDegrees;
 	}
 }
