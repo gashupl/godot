27d721379a8d15748035924ac40ccb0108a0de07
Piotr Gaszewski <piotrek.gaszewski@hotmail.com>
2024-10-27 09:17:00 +0100

    Changed shift value

--- lesson01/monster_scene.cs
+++ lesson01/monster_scene.cs
@@ -10,25 +10,25 @@ public partial class monster_scene : Sprite2D
 	Label label; 
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
-    {
-        child = this.GetNode<Sprite2D>("MonsterChildSprite");
-        label = this.GetNode<Label>("Label");
-        //child.Position = new Vector2(100, 100);
-        child.GlobalPosition = vector_calculator.CalculatePointOnCircle(30, this.Position, angleInDegrees); 
+	{
+		child = this.GetNode<Sprite2D>("MonsterChildSprite");
+		label = this.GetNode<Label>("Label");
+		//child.Position = new Vector2(100, 100);
+		child.GlobalPosition = vector_calculator.CalculatePointOnCircle(30, this.Position, angleInDegrees); 
 		label.Text = $"Parent-x: {this.Position.X}, Parent-y: {this.Position.Y} Child-x: {child.Position.X}, Child-y: {child.Position.Y} angleInDegrees: {angleInDegrees}";
-    }
+	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	//Run multiple times (~60) every second
 	public override void _Process(double delta)
 	{
 		label.GlobalPosition = new Vector2(10, 10);
-        float SHIFT = 5;
+		float SHIFT = 1;
 		if (Input.IsKeyPressed(Key.W))
 		{		
 			this.Position += new Vector2(0, -SHIFT);
 			child.GlobalPosition = vector_calculator.CalculatePointOnCircle(radius, this.Position, IncreaseAngle());
-        }
+		}
 		if (Input.IsKeyPressed(Key.S))
 		{
 			this.Position += new Vector2(0, SHIFT);
@@ -48,7 +48,7 @@ public partial class monster_scene : Sprite2D
 		label.Text = $"Parent-x: {this.Position.X}, Parent-y: {this.Position.Y} Child-x: {child.Position.X}, Child-y: {child.Position.Y} angleInDegrees: {angleInDegrees}";
 	}
 
-    public int IncreaseAngle()
+	public int IncreaseAngle()
 	{
 		angleInDegrees += 1;
 		if(angleInDegrees >= 360)
